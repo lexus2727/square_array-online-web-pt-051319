fddb5eb3721c287bd2eaf45252f01a5e89029816
Candice Parker <lexus2424@gmail.com>
2019-05-18 01:15:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  #creates new arrray
+  new_array = []
+  
+  array.each {|a| new_array.push(a*a)}
+  return new_array
+  end
 end
+  
+arr =[1, 2, 3, 4, 5]
+square_array(arr)
