2dd458b7c76100f342c3d1a0201cceaf1f50581f
Candice Parker <lexus2424@gmail.com>
2019-05-18 01:54:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,10 +2,12 @@ def square_array(array)
   #creates new arrray
   
   new_array = []
-  
+ #iterate through array using each 
 array.each do |a|
+ #square numbers being pushed into new array 
   new_array << (a**2)
 end
 return new_array
 end
 square_array([1, 2, 4, 5])
+
